ea1b61e3fb3630b5ee25cf3e1b4e0d3330d735d6
jj <mengdaq@gmail.com>
2020-05-26 20:31:41 -0700

    Updated aliashes and yaml for ansible pizero setup

--- env.sh
+++ env.sh
@@ -1,5 +1,7 @@
 export DEFAULT_USER=jj
 
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+
 #z
 source ~/Shell/z/z.sh
 
@@ -11,4 +13,23 @@ source ~/Shell/alias.sh
 #python
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 source /usr/local/bin/virtualenvwrapper.sh
+
+#ruby
+eval "$(rbenv init -)"
+
+#android
+export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home"
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+#fastlane
+export PATH="$HOME/.fastlane/bin:$PATH"
+
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+
+nvm use 10.16.0
 
